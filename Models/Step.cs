169f49d67b1575f92df063d2b4bee637b46d41b1
Holli Davis <hollidavis19@gmail.com>
2021-09-02 14:29:33 -0600

    edited models

--- Models/Step.cs
+++ Models/Step.cs
@@ -1,10 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace allspice.Models
 {
     public class Step
     {
         public int Id{get; set;}
         public string Body{get; set;}
+        [Required]
         public string CreatorId{get; set;}
+        [Required]
         public int RecipeId{get; set;}
     }
 }
