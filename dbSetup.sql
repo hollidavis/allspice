624fa8d1ec936d111d8436585cb57b0032a5417b
Holli Davis <hollidavis19@gmail.com>
2021-09-02 14:18:44 -0600

    tables 👍

--- dbSetup.sql
+++ dbSetup.sql
@@ -13,15 +13,15 @@ CREATE TABLE recipes(
   body varchar(1000) comment 'recipes description',
   cookTime int comment 'recipes cook time',
   prepTime int comment 'recipes prep time',
-  creatorId int NOT NULL,
+  creatorId VARCHAR(255) NOT NULL,
   FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
 ) default charset utf8;
 
 CREATE TABLE steps(
   id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
   body varchar(500) comment 'step description',
-  creatorId int NOT NULL,
-  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+  creatorId VARCHAR(255) NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE,
   recipeId int NOT NULL,
   FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
 ) default charset utf8;
