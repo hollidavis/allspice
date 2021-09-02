06fac616ab85d428996feb4fc830b2f3b6a6aed7
Holli Davis <hollidavis19@gmail.com>
2021-09-02 14:11:23 -0600

    created files & recipes table

--- dbSetup.sql
+++ dbSetup.sql
@@ -6,3 +6,14 @@ CREATE TABLE IF NOT EXISTS accounts(
   email varchar(255) COMMENT 'User Email',
   picture varchar(255) COMMENT 'User Picture'
 ) default charset utf8 COMMENT '';
+
+CREATE TABLE recipes(
+  id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
+  title varchar(255) comment 'recipes title',
+  body varchar(255) comment 'recipes description',
+  cookTime int comment 'recipes cook time',
+  prepTime int comment 'recipes prep time',
+  creatorId int NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8;
+
