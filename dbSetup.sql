ced68c0554f90e82510de25813dd256db94fd92b
Holli Davis <hollidavis19@gmail.com>
2021-09-02 14:14:59 -0600

    steps table

--- dbSetup.sql
+++ dbSetup.sql
@@ -10,10 +10,19 @@ CREATE TABLE IF NOT EXISTS accounts(
 CREATE TABLE recipes(
   id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
   title varchar(255) comment 'recipes title',
-  body varchar(255) comment 'recipes description',
+  body varchar(1000) comment 'recipes description',
   cookTime int comment 'recipes cook time',
   prepTime int comment 'recipes prep time',
   creatorId int NOT NULL,
   FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+) default charset utf8;
+
+CREATE TABLE steps(
+  id int NOT NULL primary key AUTO_INCREMENT comment 'primary key',
+  body varchar(500) comment 'step description',
+  creatorId int NOT NULL,
+  FOREIGN KEY (creatorId) REFERENCES accounts(id) ON DELETE CASCADE
+  recipeId int NOT NULL,
+  FOREIGN KEY (recipeId) REFERENCES recipes(id) ON DELETE CASCADE
 ) default charset utf8;
 
