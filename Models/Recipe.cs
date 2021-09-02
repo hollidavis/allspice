439a5f9ef2b856a9477cb99f00928abc17f1eab2
Holli Davis <hollidavis19@gmail.com>
2021-09-02 14:23:43 -0600

    profile & recipe model done

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -2,6 +2,15 @@ namespace allspice.Models
 {
     public class Recipe
     {
-        
+        public int Id{get; set;}
+        [Required]
+        [MinLength(3)]
+        [MaxLength(25)]
+        public string Title{get; set;}
+        public string Body{get; set;}
+        public int CookTime{get; set;}
+        public int PrepTime{get; set;}
+        public string CreatorId{get; set;}
+        public Profile Creator { get; set; }
     }
 }
